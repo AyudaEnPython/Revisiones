30579e3c48816cbad7eb63224d7256b69888e1ee
leugimkm <leugim911@hotmail.com>
2022-05-24 01:38:05 -0500

    update

--- code_reviews/sumar_tiempos/main_refactored.py
+++ code_reviews/sumar_tiempos/main_refactored.py
@@ -2,18 +2,17 @@
 """
 from datetime import timedelta
 from typing import List
+# pip install prototools
 from prototools.validators import validate_time
 
 
 def sumar_hora(tiempos: List[str]) -> str:
-    t = timedelta()
-    for tiempo in tiempos:
-        h, m, s = [int(s) for s in tiempo.split(":")]
-        t += timedelta(hours=h, minutes=m, seconds=s)
-    return str(t)
+    return sum([
+        timedelta(hours=t.hour, minutes=t.minute, seconds=t.second)
+        for t in tiempos
+    ], timedelta())
 
 
 if __name__ == "__main__":
     tiempos = ["01:30:50", "04:20:30", "02:10:10"]
-    tiempos = [validate_time(t) for t in tiempos]
-    print(sumar_hora(tiempos)) # output: 8:01:30
+    print(sumar_hora([validate_time(t) for t in tiempos])) # output: 8:01:30
