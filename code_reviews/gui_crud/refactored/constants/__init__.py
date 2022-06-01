462fe7a5d015d5c8fd33ad667acb613622587a34
leugimkm <leugim911@hotmail.com>
2022-05-31 22:40:52 -0500

    update

--- code_reviews/gui_crud/refactored/constants/__init__.py
+++ code_reviews/gui_crud/refactored/constants/__init__.py
@@ -12,9 +12,22 @@ CFG: Configuration = {
         "cursor": "hand1",
         "overrelief": "raise",
     },
+    "btn_cancel": {
+        "fg": "white",
+        "bg": "#E81123",
+        "bd": 2,
+        "relief": "flat",
+        "cursor": "hand1",
+        "overrelief": "raise",
+    },
     "lbl": {
         "font": (8),
         "fg": "white",
         "bg": "#314252",
+    },
+    "ent": {
+        "font": ("Arial", 12),
+        "bg": "#E7E7E7",
+        "relief": "flat",
     }
 }
