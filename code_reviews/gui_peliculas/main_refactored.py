f0a30ea77d9fa3593fe8c666ddb6f5d31e521e79
leugimkm <leugim911@hotmail.com>
2022-06-14 21:56:44 -0500

    u: added label, changed function name

--- code_reviews/gui_peliculas/main_refactored.py
+++ code_reviews/gui_peliculas/main_refactored.py
@@ -2,28 +2,32 @@
 """
 from tkinter import Tk, Button, Label, Frame
 
+titulo = "Streaming Movie"
 generos = "Accion", "Drama", "Comedia"
 peliculas = (
-    "La mascara", "El señor de los cielos", "Jumanji",
-    "juegos de la muerte", "la momia ", "terminator",
+    "La Máscara", "El Señor de los Cielos", "Jumanji",
+    "Juegos de la Muerte", "La Momia ", "Terminator",
 )
 
 
-def setup_buttons(root, w=25, h=12):
+def setup_ui(root, w=25, h=12):
     genre = Frame(root)
     movies = Frame(root)
+    Label(root, text=titulo).grid(row=0, column=0)
     for i, genero in enumerate(generos):
-        Button(genre, text=genero, width=9, height=2).grid(row=1, column=i)
+        Button(
+            genre, text=genero, width=9, height=2
+        ).grid(row=1, column=i)
     for i, pelicula in enumerate(peliculas):
         Button(
             movies, text=pelicula, width=w, height=h,
         ).grid(row=2 + i // 2, column=i % 2, padx=15, pady=10)
-    genre.grid(row=0, column=0)
-    movies.grid(row=1, column=1)
+    genre.grid(row=1, column=0)
+    movies.grid(row=2, column=1)
 
 
 if __name__ == """__main__""":
     root = Tk()
     root.geometry("1000x700")
-    setup_buttons(root)
+    setup_ui(root)
     root.mainloop()
