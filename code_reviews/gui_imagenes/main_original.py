1f918f08ce54b4a1b1600d878b765c7f4afa7802
leugimkm <leugim911@hotmail.com>
2024-01-20 18:24:55 -0500

    update

---
+++ code_reviews/gui_imagenes/main_original.py
@@ -0,0 +1,29 @@
+from tkinter import *
+
+root = Tk()
+root.geometry("400x300")
+
+Frame =Frame(root)
+
+Frame.pack()
+
+
+foto1 = PhotoImage(file="images/pizza.gif")
+foto2 = PhotoImage(file="images/tomahawk.gif")
+foto3 = PhotoImage(file="images/jamon.gif")
+
+
+Label(root, image=foto1).pack()
+Label(root, image=foto2).pack()
+Label(root, image=foto3).pack()
+
+
+Checkbutton(root, text="Pizza").pack(side="right")
+Checkbutton(root, text="Tomahaw").pack(side="right")
+Checkbutton(root, text="Jamon").pack(side="right")
+
+
+
+
+
+root.mainloop()
---
+++ code_reviews/gui_imagenes/main_refactored.py
@@ -0,0 +1,34 @@
+from tkinter import Checkbutton, Frame, IntVar, Label, PhotoImage, Tk
+
+ITEMS = ("pizza", "tomahawk", "jamon")
+
+
+class Card(Frame):
+
+    def __init__(self, master, filename, orientation):
+        super().__init__(master)
+        self.value = IntVar() 
+        self.photo = PhotoImage(file=f"images/{filename}.gif")
+        Label(self, image=self.photo).pack()
+        Checkbutton(
+            self, text=filename.capitalize(), variable=self.value
+        ).pack(side=orientation)
+
+
+class App(Tk):
+
+    def __init__(self):
+        super().__init__()
+        self.cards = []
+        self.setup_ui()
+
+    def setup_ui(self):
+        for item in ITEMS:
+            card = Card(self, item, "right")
+            card.pack(side="left")
+            self.cards.append(card)
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
