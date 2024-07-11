660c875c67fa80dce430a9824446d10d9624d286
leugimkm <leugim911@hotmail.com>
2024-07-10 23:07:04 -0500

    update

--- code_reviews/tabla_de_multiplicar/main_refactored.py
+++ code_reviews/tabla_de_multiplicar/main_refactored.py
@@ -3,16 +3,10 @@
 start = 1
 end = 10
 
-print("|", end="")
 for i in range(start, end+1):
-    print(f"tabla del {i:^3}", end=" | ")
-print("\r")
-print("+", end="")
-for i in range(start, end+1):
-    print(f" {'-'*11} ", end=" + ")
+    print(f"tabla del {i:^3}", end=" ")
 print("\r")
 for i in range(1, 11):
-    print("|", end="")
     for j in range(start, end+1):
-        print(f"{j:>2} x {i:>2} = {i*j:^3}", end=" | ")
+        print(f"{j:>2} x {i:>2} = {i*j:^3}", end=" ")
     print()
